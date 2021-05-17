b4d2780bd86fcb07471fa20ab41960791cb5ed26
Jackson <jacksono7874@gmail.com>
2021-05-17 13:25:03 -0600

    final css touches, clear and delete buttons functionallity

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const display = document.getElementById('display')
+const clearButton = document.getElementById('clear')
+const deleteButton = document.getElementById('delete')
+const decimalButton = document.getElementById('decimal')
+const equalsButton = document.getElementById('equals')
+const operatorButtons = document.querySelectorAll('operator-buttons')
+const numberButtons = document.getElementById('number-buttons')
+
 function add(x, y) {
     return x + y;
 }
@@ -14,6 +22,7 @@ function divide(x, y) {
     return x / y;
 }
 
+// dont work
 function operate(operator, x, y) {
     if (operator == multiply) {
         multiply()
@@ -22,5 +31,22 @@ function operate(operator, x, y) {
         console.log('wrong')
     }
 }
+
+clearButton.addEventListener('click', clearDisplay)
+function clearDisplay() {
+    while (display.firstChild) {
+        display.removeChild(display.firstChild)
+    }
+}
+
+deleteButton.addEventListener('click', deleteLastInput)
+function deleteLastInput() {
+    if (display.childElementCount == 0) {
+        return;
+    }
+    display.lastElementChild.remove();
+}
+
+console.log(display)
 
 console.log(operate(multiply, 6,6))
