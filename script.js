5236f7fb113dcca19ffed86b615428bf2af7b69a
Jackson <jacksono7874@gmail.com>
2021-05-20 14:03:32 -0600

    functional buttons

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const clearButton = document.getElementById('clear') // has listener
 const deleteButton = document.getElementById('delete') // has listener
 const decimalButton = document.getElementById('decimal') // has listener
 const equalsButton = document.getElementById('equals')
-const operatorButtons = document.querySelectorAll('operator-buttons')
-const numberButtons = document.querySelectorAll('number-buttons')
+const operatorButtons = document.querySelectorAll('[data-operator-buttons]')
+const numberButtons = document.querySelectorAll('[data-number-buttons]')
 
 function add(x, y) {
     return x + y;
@@ -22,23 +22,40 @@ function divide(x, y) {
     return x / y;
 }
 
-decimalButton.addEventListener('click', createDecimal);
-function createDecimal() {
-    display.textContent += decimalButton.textContent;
-    decimalButton.removeEventListener('click', createDecimal);
-};
-
 clearButton.addEventListener('click', clearDisplay);
 function clearDisplay() {
-    while (display.firstChild) {
-        display.removeChild(display.firstChild)
-    }
+    display.textContent = ""
+    decimalButton.addEventListener('click', createDecimal);
 }
 
 deleteButton.addEventListener('click', deleteLastInput);
 function deleteLastInput() {
-    if (display.childElementCount == 0) {
-        return;
+    let displayValue = display.textContent;
+    display.textContent = displayValue.replace(displayValue.charAt(displayValue.length - 1), '');
+    if (displayValue.search('.')) {
+        decimalButton.removeEventListener('click', createDecimal);
     }
-    display.lastElementChild.remove();
+}
+
+decimalButton.addEventListener('click', createDecimal);
+function createDecimal() {
+    display.textContent += "."
+    decimalButton.removeEventListener('click', createDecimal);
+};
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => updateDisplayValue(button.textContent))
+);
+function updateDisplayValue(buttonNumber) {
+    display.textContent += buttonNumber;
+}
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => updateDisplayOperator(button.textContent))
+);
+function updateDisplayOperator(buttonValue) {
+    if (buttonValue == '÷') {
+        buttonValue = '/'
+    }
+    display.textContent += buttonValue;
 }
